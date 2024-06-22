5cd7698baf5a505617f0376d45cfe74eab083b1e
dimple <dsaraogi2000@gmail.com>
2024-06-22 20:39:41 +0545

    adjusted window size

--- src/utilities/showCards.ts
+++ src/utilities/showCards.ts
@@ -10,14 +10,16 @@ import { Game } from "../Game";
 
 export function showCards(game: Game) {
   game.plantsTypes.forEach((plant, idx) => {
+    //  Sets the default boundary
     let cardBoundary = {
       x: 20,
-      y: GRID_ROW_START_POS + 80 * idx,
+      y: GRID_ROW_START_POS + 90 * idx,
       w: 100,
       h: 60,
     };
-    let cardY = GRID_ROW_START_POS + 80 * idx;
+    let cardY = GRID_ROW_START_POS + 90 * idx;
 
+    //Draws the card
     ctx.drawImage(
       plant.card,
       0,
@@ -30,6 +32,15 @@ export function showCards(game: Game) {
       idx === game.selectedPlant ? cardBoundary.h + 8 : cardBoundary.h
     );
 
+    // Adds the cost of the plant
+    ctx.font = "23px Creepster";
+    ctx.fillText(
+      plant.blueprint.cost.toString(),
+      cardBoundary.x + cardBoundary.w - 32,
+      cardBoundary.y + cardBoundary.h - 18
+    );
+
+    // Clicked plant is selected from the card
     if (isCollided(mouseStatus, cardBoundary) && mouseStatus.clicked) {
       game.selectedPlant = idx;
     }
