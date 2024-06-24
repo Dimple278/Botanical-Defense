9d0aa09c34136c16c02a581d1a407cb31884d815
dimple <dsaraogi2000@gmail.com>
2024-06-24 14:32:19 +0545

    add loading screen

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import Game from "./Game.ts";
-import { loadImages } from "./constants.ts";
+import { loadImages, loading } from "./constants.ts";
 
 window.onload = async () => {
   await loadImages();
+  if (loading instanceof HTMLElement) {
+    loading.style.display = "none";
+  } else {
+    console.error("Loading element is not found or is not an HTMLElement");
+  }
   const game = new Game();
   game.init();
 };
