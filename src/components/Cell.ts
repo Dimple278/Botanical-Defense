dd7f9975532f5d4dcad1c832715d407ee429c1bb
dimple <dsaraogi2000@gmail.com>
2024-06-24 22:31:50 +0545

    remove grid black boundary

--- src/components/Cell.ts
+++ src/components/Cell.ts
@@ -1,4 +1,4 @@
-import { mouseStatus } from "../constants";
+import { mouseStatus } from "../constants/constants";
 import { isCollided } from "../utilities/collision";
 
 export default class Cell {
@@ -13,11 +13,16 @@ export default class Cell {
     this.h = h;
   }
   draw(ctx: CanvasRenderingContext2D) {
-    ctx.strokeStyle = "black";
-    ctx.fillStyle = "#0009";
+    ctx.strokeStyle = "rgba(0, 0, 0, 0.0)";
+    ctx.fillStyle = "#0008";
     ctx.strokeRect(this.x, this.y, this.w, this.h);
     if (mouseStatus.x && mouseStatus.y && isCollided(this, mouseStatus)) {
-      ctx.fillRect(this.x, this.y, this.w, this.h);
+      const radius = 55; // Radius of the circle
+      const centerX = this.x + this.w / 2;
+      const centerY = this.y + this.h / 2;
+      ctx.beginPath();
+      ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
+      ctx.fill();
     }
   }
 }
